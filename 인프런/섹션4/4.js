da393d2c7e905eb4be2a3958e002afd3f96e357c
choiwonseokgit <cs0328@hanmail.net>
2024-03-26 01:08:26 +0900

    Feat: 인프런 섹션4 4번 풀이 수정 및 5번 풀이 완료

--- 인프런/섹션4/4.js
+++ 인프런/섹션4/4.js
@@ -1,56 +1,46 @@
 function solution(money, arr) {
-  arr.sort((a, b) => a[0] / 2 + a[1] - (b[0] / 2 + b[1]));
-  console.log(arr);
-  let cnt = 0;
-  let numSum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    //일단 더한다
-    numSum += arr[i][0] + arr[i][1];
-    //토탈이 넘으면 그때 이전값과 비교해서
-    console.log(numSum, money);
-    if (numSum > money) {
-      const prevDivideCase =
-        numSum -
-        (arr[i - 1][0] + arr[i - 1][1]) +
-        (arr[i - 1][0] / 2 + arr[i - 1][1]);
-      const currDivideCase =
-        numSum - (arr[i][0] + arr[i][1]) + arr[i][0] / 2 + arr[i][1];
-      const smallerSum =
-        prevDivideCase < currDivideCase ? prevDivideCase : currDivideCase;
+  let cntMax = 0;
+  arr.sort((a, b) => a[0] + a[1] - (b[0] + b[1]));
 
-      console.log("smallerSum", smallerSum);
-      if (smallerSum <= money) {
-        cnt += 1;
+  for (let i = 0; i < arr.length; i++) {
+    let total = 0;
+    let cnt = 0;
+    arr.forEach((el, idx) => {
+      if (idx === i) {
+        total += el[0] / 2 + el[1];
+      } else {
+        total += el[0] + el[1];
       }
-      break;
-    }
-    //안넘으면 카운트
-    cnt += 1;
+
+      if (total <= money) cnt += 1;
+      else return;
+    });
+    cntMax = Math.max(cntMax, cnt);
   }
-  return cnt;
+  return cntMax;
 }
 
-console.log(
-  solution(41, [
-    [8, 6],
-    [2, 2],
-    [4, 3],
-    [4, 5],
-    [12, 1],
-  ])
-);
-console.log(
-  solution(41, [
-    [8, 6],
-    [2, 2],
-    [12, 1],
-    [4, 5],
-    [4, 3],
-  ])
-);
+// console.log(
+//   solution(41, [
+//     [8, 6],
+//     [2, 2],
+//     [4, 3],
+//     [4, 5],
+//     [12, 1],
+//   ])
+// );
+// console.log(
+//   solution(41, [
+//     [8, 6],
+//     [2, 2],
+//     [12, 1],
+//     [4, 5],
+//     [4, 3],
+//   ])
+// );
 console.log(
   solution(33, [
-    [2, 12],
+    [2, 12], //13 + 12 + 9 + 10
     [8, 4],
     [6, 6],
     [6, 7],
---
+++ 인프런/섹션4/5.js
@@ -0,0 +1,23 @@
+function solution(k, arr) {
+  const sumArr = [];
+  function combination(startIdx, currArr) {
+    // console.log(startIdx, currArr);
+    if (currArr.length === k) {
+      sumArr.push(currArr.reduce((acc, el) => acc + el, 0));
+      return;
+    }
+
+    for (let i = startIdx; i < arr.length; i++) {
+      combination(i + 1, [...currArr, arr[i]]);
+    }
+  }
+
+  combination(0, []);
+
+  sumArr.sort((a, b) => b - a);
+  const deDuplicatedArr = Array.from(new Set(sumArr));
+  return deDuplicatedArr[k - 1] ? deDuplicatedArr[k - 1] : -1;
+}
+
+console.log(solution(3, [13, 15, 34, 23, 45, 65, 33, 11, 26, 42]));
+// console.log(solution(3, [1, 2, 3, 4, 5]));
