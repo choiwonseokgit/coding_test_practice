8b5dc50f017208495a75a3d1b87282984da84fa1
choiwonseokgit <cs0328@hanmail.net>
2024-05-02 22:47:56 +0900

    인프런 섹션5 1번 풀이

--- 인프런/섹션5/1.js
+++ 인프런/섹션5/1.js
@@ -3,4 +3,24 @@ function solution(arr1, arr2) {
   return newArr;
 }
 
-console.log(solution([1, 3, 5], [2, 3, 6, 7, 9]));
+function solution_teacher(arr1, arr2) {
+  const answer = [];
+  const n = arr1.length;
+  const m = arr2.length;
+  let p1 = (p2 = 0);
+  while (p1 < n && p2 < m) {
+    if (arr1[p1] <= arr2[p2]) {
+      answer.push(arr1[p1]);
+      p1 += 1;
+    } else {
+      answer.push(arr2[p2]);
+      p2 += 1;
+    }
+  }
+  while (p1 < n) answer.push(arr1[p1++]);
+  while (p2 < m) answer.push(arr2[p2++]);
+
+  return answer;
+}
+
+console.log(solution_teacher([1, 3, 5], [2, 3, 6, 7, 9]));
