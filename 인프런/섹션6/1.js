e1c592b2e377f383cb9d1e6a75fb87e9ad081d0c
choiwonseokgit <cs0328@hanmail.net>
2024-06-13 01:41:20 +0900

    인프런 섹션6 1번 풀이

--- 인프런/섹션6/1.js
+++ 인프런/섹션6/1.js
@@ -9,7 +9,8 @@ function solution(str) {
     if (s === right && stack.length !== 0) stack.pop();
     else return "NO";
   }
-  return "YES";
+  // left가 남아있을때 판단
+  return stack.length === 0 ? "YES" : "NO";
 }
 
 console.log(solution("(()(()))(()"));
