86eefcdfec1e6a68d8ba0d5080daa10f256400d0
choiwonseokgit <cs0328@hanmail.net>
2024-06-15 01:12:24 +0900

    백준 1654 추가 풀이

--- 백준/이분탐색/1654.js
+++ 백준/이분탐색/1654.js
@@ -1,31 +1,64 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const [len, targetCnt] = input[0].split(" ").map((el) => +el);
+const arr = input.slice(1).map((el) => +el);
 
-const targetNum = parseInt(input[0].split(" ")[1]);
-const cableArr = input.slice(1).map((el) => parseInt(el, 10));
-const maxLen = Math.max(...cableArr);
+arr.sort((a, b) => a - b);
 
-let curLen = 0;
+let answer;
+let lt = 1;
+let rt = Math.max(...arr);
 
-function binarySearch(startLen, endLen) {
-  const midLen = Math.floor((startLen + endLen) / 2);
-
+function count(mid) {
   let cnt = 0;
-
-  cableArr.forEach((cable) => {
-    const num = Math.floor(cable / midLen);
-    cnt += num;
+  arr.forEach((el) => {
+    const cutStickNum = parseInt(el / mid);
+    cnt += cutStickNum;
   });
-
-  if (cnt >= targetNum) {
-    curLen = Math.max(curLen, midLen);
-    if (startLen >= endLen) return;
-    return binarySearch(midLen + 1, endLen);
-  }
-
-  return binarySearch(startLen, midLen - 1);
+  return cnt;
 }
 
-binarySearch(1, maxLen);
+while (lt <= rt) {
+  const mid = parseInt((lt + rt) / 2);
+  const cnt = count(mid);
+  if (cnt >= targetCnt) {
+    answer = mid;
+    lt = mid + 1;
+  } else {
+    rt = mid - 1;
+  }
+}
 
-console.log(curLen);
+console.log(answer);
+
+// const fs = require("fs");
+// const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+// const targetNum = parseInt(input[0].split(" ")[1]);
+// const cableArr = input.slice(1).map((el) => parseInt(el, 10));
+// const maxLen = Math.max(...cableArr);
+
+// let curLen = 0;
+
+// function binarySearch(startLen, endLen) {
+//   const midLen = Math.floor((startLen + endLen) / 2);
+
+//   let cnt = 0;
+
+//   cableArr.forEach((cable) => {
+//     const num = Math.floor(cable / midLen);
+//     cnt += num;
+//   });
+
+//   if (cnt >= targetNum) {
+//     curLen = Math.max(curLen, midLen);
+//     if (startLen >= endLen) return;
+//     return binarySearch(midLen + 1, endLen);
+//   }
+
+//   return binarySearch(startLen, midLen - 1);
+// }
+
+// binarySearch(1, maxLen);
+
+// console.log(curLen);
