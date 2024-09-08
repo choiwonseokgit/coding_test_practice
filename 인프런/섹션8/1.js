4e24f4e182bec8489b3b90597bd2ce3860521131
choiwonseokgit <cs0328@hanmail.net>
2024-09-08 21:02:41 +0900

    인프런 섹션8 1번 풀이

--- 인프런/섹션8/1.js
+++ 인프런/섹션8/1.js
@@ -1,34 +1,16 @@
-function solution_teacher(n) {
+function solution(targetNum) {
+  let answer = "";
+
   function DFS(num) {
-    if (num === 0) return;
-    else {
-      DFS(num - 1);
-      console.log(num);
-    }
+    if (num > targetNum) return;
+    answer += num + " ";
+
+    DFS(num + 1);
   }
 
-  DFS(n);
+  DFS(1);
+
+  return answer;
 }
 
-function solution(n) {
-  let answerStr = "";
-  function getNumbers(startNum, selectedArr) {
-    if (selectedArr.length === n) {
-      answerStr += selectedArr.join(" ");
-      return;
-    }
-
-    return getNumbers(startNum + 1, [...selectedArr, startNum]);
-  }
-
-  getNumbers(1, []);
-
-  return answerStr;
-}
-// 1 []
-// 2 [1]
-// 3 [1,2]
-// 4 [1,2,3]
-
-// console.log(solution(10));
-console.log(solution_teacher(3));
+console.log(solution(3));
