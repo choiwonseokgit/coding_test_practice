3177c1b5f3a16059ce441b17104ed523b67963d6
choiwonseokgit <cs0328@hanmail.net>
2025-01-03 13:29:59 +0900

    코딩테스트 합격자 되기 - 68

---
+++ 코딩테스트 합격자 되기/68.js
@@ -0,0 +1,14 @@
+function solution(n) {
+  let answer = 1;
+
+  while (n !== 1) {
+    if (n % 2 === 0) {
+      n /= 2;
+    } else {
+      n -= 1;
+      answer++;
+    }
+  }
+
+  return answer;
+}
