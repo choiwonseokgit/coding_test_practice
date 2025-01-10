194c28d808c12bcd536976db84c1e7d4dce4d72d
choiwonseokgit <cs0328@hanmail.net>
2025-01-10 15:48:49 +0900

    코딩테스트 합격자 되기 - 74, 75 풀이

---
+++ 코딩테스트 합격자 되기/75.js
@@ -0,0 +1,17 @@
+function solution(triangle) {
+  const n = triangle.length;
+
+  const dp = Array.from({ length: n }, () => Array(n).fill(0)); // n * n dp 배열
+
+  for (let i = 0; i < n; i++) {
+    dp[n - 1][i] = triangle[n - 1][i];
+  }
+
+  for (let i = n - 2; i >= 0; i--) {
+    for (let j = 0; j <= i; j++) {
+      dp[i][j] = Math.max(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j];
+    }
+  }
+
+  return dp[0][0];
+}
